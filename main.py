c91d4fe8eed5890112ce471e6ec4c2fec38d8c6c
Jason <jasondavidoff85@gmail.com>
2019-02-28 17:29:16 -0500

    added first functions

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("nice")
+gameBoard = []
 
+def createGameBoard():
+    for i in range(0,9):
+        gameBoard.append([0,0,0,0,0,0,0,0,0])
+
+def printBoard():
+    for i in gameBoard:
+        print(i)
+
+def main():
+    createGameBoard()
+    printBoard()
+
+if __name__ == '__main__':
+    main()
