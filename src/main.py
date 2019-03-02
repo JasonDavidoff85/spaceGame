fb2bc8bbf4a57988cb29566cf6f435a3021f7523
Jason <jasondavidoff85@gmail.com>
2019-03-01 20:33:56 -0500

    added __pycache__

--- src/main.py
+++ src/main.py
@@ -1,17 +1,40 @@
-gameBoard = []
+from gunship import Gunship
 
-def createGameBoard():
-    for i in range(0,9):
-        gameBoard.append([0,0,0,0,0,0,0,0,0])
+class GameBoard:
+	def __init__(self, mapSize):
+		self.gameBoard = []
+		for i in range(0,int(mapSize[0])):
+			self.gameBoard.append([0]*int(mapSize[-1]))
 
-def printBoard():
-    for i in gameBoard:
-        print(i)
+	def isSpaceAvailible(xy):
+		return 0
+
+	def placeAstroids(self):
+		return 0
+
+	#==== Utility Functions =============
+	def printBoard(self):
+		for i in self.gameBoard:
+			print(i)
+
+def setupGame():
+	mapSize = "9x9"
+	#==set up game here==
+	Map = GameBoard(mapSize) #create map
+	Map.printBoard()
+	#create astroids
+	#create refulling stations
+	#create rocket locations	
+	#create abondoned ships (essentially chests)
+	#create enemies
+	#place enemines
+	samus = Gunship() #create Samus
+	samus.printStats()
+	#place samus
+	main()
 
 def main():
-    createGameBoard()
-    printBoard()
-
+	return 1
 
 if __name__ == '__main__':
-    main()
+    setupGame()
