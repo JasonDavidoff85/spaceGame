fe2403a0119cdca03f2f9918d687c25f3ce43a80
Jason Davidoff <jasondavidoff85@gmail.com>
2019-03-07 15:18:51 -0500

    added counters for creation of enemies, enemy remaining method, and changed id to enemy type

--- src/spacePirates.py
+++ src/spacePirates.py
@@ -1,15 +1,31 @@
-class spacePirates:
+from itertools import count
+
+class SpacePirates:
+	pirate_ids = count(0)
+	captain_ids = count(0)
+
+	piratesRemaining = 0
+	captainsRemaining = 0
+
 	def __init__(self, enemyNum):
-		# if enemy num is odd, it is a bady
-		if ((enemyNum % 2) == 0):
-			self.enemyNum = enemyNum
+		global piratesRemaining
+		global captainsRemaining
+
+		if (enemyNum == "Grunt"):
+			self.id = next(self.pirate_ids)
+			self.enemyType = enemyNum
 			self.shields = 75
 			self.health = 40
 			self.rockets = 5
-		elif ((enemyNum % 1) != 0):
-			self.enemyNum = enemyNum
+			piratesRemaining = self.id
+
+		elif (enemyNum == "Captain"):
+			self.id = next(self.captain_ids)
+			self.enemyType = enemyNum
 			self.shields = 120
 			self.health = 60
 			self.rockets = 8
-		else:
-			raise Exception('ummmmm....')
+			captainsRemaining = self.id
+
+	def enemiesRemaining():
+		return ([piratesRemaining + 1,captainsRemaining + 1])
